04882799b2382889de3f2a7761f3b9dd4304b3e4
muxthafa <musthafamohd0@gmail.com>
2021-08-21 23:44:06 +0530

    Added: use case 7

--- EmpWage.java
+++ EmpWage.java
@@ -1,40 +1,56 @@
 package com.employee;
 
 public class EmpWage {
-	public static final int IS_PART_TIME =1;	//constant
-	public static final int IS_FULL_TIME =2;		
-	public static final int EMP_RATE_PER_HOUR=20;
-	public static final int NUM_WORKING_DAYS=20;
-	public static final int MAX_HRS_IN_MONTH=100;
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		System.out.println("Welcome to Employee wage Computation program!");
-		
-		int Emp_Hrs=0;					//local variable
-		int Emp_Wage=0;
-		int Total_Wage=0;
-		int TotalEmpHrs=0;
-		int TotalWorkingDays=0;
-		while(TotalEmpHrs<MAX_HRS_IN_MONTH && TotalWorkingDays<NUM_WORKING_DAYS)
-		{
-			TotalWorkingDays+=1;
-			int empCheck=(int) (Math.floor(Math.random()*10)%3);
-			switch (empCheck) 
+			public static final int IS_PART_TIME=1;
+			public static final int IS_FULL_TIME =2;
+			
+			private final String company;		//members of class
+			private final int EMP_RATE_PER_HOUR;
+			private final int NUM_WORKING_DAYS;
+			private final int MAX_HRS_IN_MONTH;
+			
+			//constructor to initialize members of a class
+			public EmpWage(String company,int EMP_RATE_PER_HOUR,int NUM_WORKING_DAYS,int MAX_HRS_IN_MONTH)
 			{
-				case  IS_FULL_TIME: Emp_Hrs=16;break;
-				case IS_PART_TIME: Emp_Hrs=8;break;
-				default: Emp_Hrs=0;
+				this.company=company;
+				this.EMP_RATE_PER_HOUR=EMP_RATE_PER_HOUR;
+				this.NUM_WORKING_DAYS = NUM_WORKING_DAYS;
+				this.MAX_HRS_IN_MONTH=MAX_HRS_IN_MONTH;
+				
 			}
-			TotalEmpHrs+=Emp_Hrs;
-			Emp_Wage=Emp_Hrs*EMP_RATE_PER_HOUR ;
-			Total_Wage+=Emp_Wage;
-			System.out.println("Employee wage is "+Emp_Wage);
-		}
-		System.out.println("Total wage is "+Total_Wage);
-		System.out.println("Total working hours is "+TotalEmpHrs);
-		System.out.println("Total working days is "+TotalWorkingDays);
-	}
+			
+			public static void main(String[] args) 
+			{
+
+				//local variables
+				int Emp_Hrs=0;
+				int Emp_Wage=0;
+				int Total_Wage=0;
+				int TotalEmpHrs=0;
+				int TotalWorkingDays=0;
+				
+				EmpWage emp1 = new EmpWage("PaceX",20,20,100);//object creation
+				
+				// Computation
+				while(TotalEmpHrs<emp1.MAX_HRS_IN_MONTH && TotalWorkingDays<emp1.NUM_WORKING_DAYS)
+				{
+					TotalWorkingDays+=1;
+					int empCheck=(int) (Math.floor(Math.random()*10)%3);
+					switch (empCheck) 
+					{
+						case  IS_FULL_TIME: Emp_Hrs=16;break;
+						case IS_PART_TIME: Emp_Hrs=8;break;
+						default: Emp_Hrs=0;
+					}
+					TotalEmpHrs+=Emp_Hrs;
+					Emp_Wage=Emp_Hrs*emp1.EMP_RATE_PER_HOUR ;
+					Total_Wage+=Emp_Wage;
+					System.out.println("Employee wage for the company "+emp1.company+" is "+Emp_Wage);
+				}
+				System.out.println("Total wage is "+Total_Wage);
+				System.out.println("Total working hours is "+TotalEmpHrs);
+				System.out.println("Total working days is "+TotalWorkingDays);
+			}
 
 }
 
