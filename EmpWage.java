48a0cb705a5f8732425f8af8e366af5aecfad355
muxthafa <musthafamohd0@gmail.com>
2021-08-21 23:24:40 +0530

    Added: use case 4

--- EmpWage.java
+++ EmpWage.java
@@ -1,7 +1,9 @@
 package com.employee;
 
 public class EmpWage {
-
+	public static final int IS_PART_TIME =1;	//constant
+	public static final int IS_FULL_TIME =2;		
+	public static final int EMP_RATE_PER_HOUR=20;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
@@ -9,14 +11,13 @@ public class EmpWage {
 		
 		int Emp_Hrs=0;					//local variable
 		int Emp_Wage=0;
-		final int IS_PART_TIME =1;
-		final int IS_FULL_TIME =2;		//constant
-		final int EMP_RATE_PER_HOUR=20;
-		double empCheck=Math.floor(Math.random()*10)%3;
-		if (empCheck==IS_FULL_TIME)		//condition to check employee is present or not
-			Emp_Hrs=16;
-		else if (empCheck==IS_PART_TIME)
-			Emp_Hrs=8;
+		
+		int empCheck=(int) (Math.floor(Math.random()*10)%3);
+		switch (empCheck) {			
+		case  IS_FULL_TIME: Emp_Hrs=16;break;
+		case IS_PART_TIME: Emp_Hrs=8;break;
+		default: Emp_Hrs=0;
+		}
 		Emp_Wage=Emp_Hrs*EMP_RATE_PER_HOUR ;	//calculating daily employee wage
 		System.out.println("Employee wage is "+Emp_Wage);
 	}
