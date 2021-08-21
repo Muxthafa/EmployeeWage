9fff386bfb08fa8fc5ba76de4222a83482936a14
muxthafa <musthafamohd0@gmail.com>
2021-08-21 22:59:49 +0530

    Added: welcom message for employee wage problem

--- EmpWage.java
+++ EmpWage.java
@@ -5,12 +5,7 @@ public class EmpWage {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to Employee wage Computation program!");
-		final int IS_FULL_TIME =1;		//constant
-		double empCheck=Math.floor(Math.random()*10)%2;
-		if (empCheck==IS_FULL_TIME)		//condition to check employee is present or not
-			System.out.println("Employee is present");
-		else
-			System.out.println("Employee is absent"); 
+		
 	}
 
 }
