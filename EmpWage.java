7805ad7711ada3b17f454edcfec7781ac50fe301
muxthafa <musthafamohd0@gmail.com>
2021-08-21 23:17:56 +0530

    Added: use case 3

--- EmpWage.java
+++ EmpWage.java
@@ -9,10 +9,13 @@ public class EmpWage {
 		
 		int Emp_Hrs=0;					//local variable
 		int Emp_Wage=0;
-		final int IS_FULL_TIME =1;		//constant
+		final int IS_PART_TIME =1;
+		final int IS_FULL_TIME =2;		//constant
 		final int EMP_RATE_PER_HOUR=20;
-		double empCheck=Math.floor(Math.random()*10)%2;
+		double empCheck=Math.floor(Math.random()*10)%3;
 		if (empCheck==IS_FULL_TIME)		//condition to check employee is present or not
+			Emp_Hrs=16;
+		else if (empCheck==IS_PART_TIME)
 			Emp_Hrs=8;
 		Emp_Wage=Emp_Hrs*EMP_RATE_PER_HOUR ;	//calculating daily employee wage
 		System.out.println("Employee wage is "+Emp_Wage);
