0b43710ff8a4fb798fa1ceae5149d84903799c6d
muxthafa <musthafamohd0@gmail.com>
2021-08-21 23:29:42 +0530

    Added: use case 5

--- EmpWage.java
+++ EmpWage.java
@@ -4,6 +4,7 @@ public class EmpWage {
 	public static final int IS_PART_TIME =1;	//constant
 	public static final int IS_FULL_TIME =2;		
 	public static final int EMP_RATE_PER_HOUR=20;
+	public static final int NUM_WORKING_DAYS=20;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
@@ -11,15 +12,22 @@ public class EmpWage {
 		
 		int Emp_Hrs=0;					//local variable
 		int Emp_Wage=0;
-		
-		int empCheck=(int) (Math.floor(Math.random()*10)%3);
-		switch (empCheck) {			
-		case  IS_FULL_TIME: Emp_Hrs=16;break;
-		case IS_PART_TIME: Emp_Hrs=8;break;
-		default: Emp_Hrs=0;
-		}
-		Emp_Wage=Emp_Hrs*EMP_RATE_PER_HOUR ;	//calculating daily employee wage
-		System.out.println("Employee wage is "+Emp_Wage);
+		int Total_Wage=0;
+		for(int day=0;day<NUM_WORKING_DAYS;day++)		//iterating to calculate total wage
+		{
+			int empCheck=(int) (Math.floor(Math.random()*10)%3);
+			switch (empCheck)
+			{
+			case  IS_FULL_TIME: Emp_Hrs=16;break;
+			case IS_PART_TIME: Emp_Hrs=8;break;
+			default: Emp_Hrs=0;
+			}
+
+			Emp_Wage=Emp_Hrs*EMP_RATE_PER_HOUR ;
+			Total_Wage+=Emp_Wage;
+			System.out.println("Employee wage is "+Emp_Wage);
+		 }
+		System.out.println("Total wage is "+Total_Wage);
 	}
 
 }
