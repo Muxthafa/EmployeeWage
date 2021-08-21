8a876a524d500ed6d233c5efb7bd300e45e4c32c
muxthafa <musthafamohd0@gmail.com>
2021-08-21 23:13:06 +0530

    Added: use case 2

--- EmpWage.java
+++ EmpWage.java
@@ -4,8 +4,22 @@ public class EmpWage {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
 		System.out.println("Welcome to Employee wage Computation program!");
 		
+		int Emp_Hrs=0;					//local variable
+		int Emp_Wage=0;
+		final int IS_FULL_TIME =1;		//constant
+		final int EMP_RATE_PER_HOUR=20;
+		double empCheck=Math.floor(Math.random()*10)%2;
+		if (empCheck==IS_FULL_TIME)		//condition to check employee is present or not
+			Emp_Hrs=8;
+		Emp_Wage=Emp_Hrs*EMP_RATE_PER_HOUR ;	//calculating daily employee wage
+		System.out.println("Employee wage is "+Emp_Wage);
 	}
 
 }
+
+		
+				
+		
